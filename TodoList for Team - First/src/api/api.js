42ada052091abc31d5499395b81a3747dc3df28a
my-U <myouth_u@naver.com>
2021-04-20 23:27:41 +0900

    api 모듈 분리

---
+++ TodoList for Team - First/src/api/api.js
@@ -0,0 +1,42 @@
+import { BASE_URL } from '../api/api.js';
+
+const option = {
+    post: (contents) => ({
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+        body: JSON.stringify(contents)
+    }),
+
+    delete: () => ({
+        method: 'DELETE',
+        headers: { 'Content-Type' : 'application/json'}
+    }),
+
+    put: (contents) => ({
+        method: 'PUT',
+        headers: { 'Content-Type' : 'application/json'},
+        body: JSON.stringify(contents)
+    })
+};
+
+const request = async (url, option = {}) => {
+    try{
+        const response = await fetch(`${BASE_URL}${url}`, option);
+        if(!response.ok) {
+            throw new Error(response.status);
+        }
+        return await response.json();
+    } catch(err) {
+        alert(`💣 Error : ${err} 💣`);
+    }
+};
+
+export const API = {
+    getTeam:(teamId) => {
+        return request(`api/teams/${teamId}`);
+    },
+
+    getTeams: () => {
+        return request(`api/teams`);
+    }
+}
